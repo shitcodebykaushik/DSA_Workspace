1d33eccea436d12ac7809c23efe327348e2d0841
Kaushik raj <algorithmunloack@gmail.com>
2024-07-08 17:58:31 +0530

    The neseted loop is done

---
+++ C++/Basic/for.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    for (int i =0; i<12; i++){
+        for (int i = 0; i<10;i++){
+            cout<<"If this is right"<<i<<endl;
+        }
+        cout<<"The value is" <<i<<endl;
+    }
+}
