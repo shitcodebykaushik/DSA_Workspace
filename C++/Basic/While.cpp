72a37a9fb0c20d7b8f7cc2eb2d361a0c350c08ef
Kaushik raj <algorithmunloack@gmail.com>
2024-07-08 18:06:41 +0530

    The while is done

---
+++ C++/Basic/While.cpp
@@ -0,0 +1,10 @@
+// The will work until the condition is satisfied  .
+#include <iostream>
+using namespace std;
+int main () {
+    int b =45;
+    int c = 349;
+    while (b>c) {
+        cout <<"The value is ok "<<endl;
+    }
+}
