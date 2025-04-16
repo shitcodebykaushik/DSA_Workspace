ba7c50a6586ad7e297c353dc920d28cd5c14c50b
shitcodebykaushik <algorithmunloack@gmail.com>
2025-04-16 21:23:34 +0530

    The swapping is done

---
+++ Core/swap.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std ;
+int main () {
+    vector<int>arr={1,22,3,30,5};
+    int a = arr[1];
+    cout<<a<<endl;
+    int b = arr[2];
+    cout<<b<<endl;
+     swap(a,b);
+    cout<<"After swap value of \n A is " <<a<<" \n B value is "<<b<<endl;
+    
+    
+} 
