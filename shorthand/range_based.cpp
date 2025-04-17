ce13125de9d05eaafbc307e8665f98b1e841ec99
shitcodebykaushik <algorithmunloack@gmail.com>
2025-04-17 15:36:19 +0530

    The range based shit is here

---
+++ shorthand/range_based.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    vector<int>nums = {1,2,4,5};
+    for(int x:nums){
+        cout<<x<<endl;
+    }
+}
+
+// This whole is the shorthand for the range based loop where it runs
+// for rach value of the x and print it into the terminal .
