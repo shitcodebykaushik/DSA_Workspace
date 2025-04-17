ef37507fc71bb1d197fe5cd70b6c067b17a75934
shitcodebykaushik <algorithmunloack@gmail.com>
2025-04-17 16:13:07 +0530

    The range based shit is here

--- shorthand/range_based.cpp
+++ shorthand/range_based.cpp
@@ -1,11 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main () {
-    vector<int>nums = {1,2,4,5};
-    for(int x:nums){
-        cout<<x<<endl;
+   int nums[] = {1,2,4,5};
+    for(int &x:nums){   // Here we did in place modification .
+         x = x+1;
     }
+    cout<<nums[1]<<endl;
+    // NO inplace modification 
+    int copy[]= {1,3,4};
+    for (int i:copy){
+        i =i+1;
+        cout<<"The value with the copy are"<<i<<endl;
+    }
+    cout<<"The value after loop remain same no change in original array"<<copy[1]<<endl;
+
 }
 
 // This whole is the shorthand for the range based loop where it runs
-// for rach value of the x and print it into the terminal .
+// for rach value of the x and print it into the terminal . Here we are copy the value .
+// If you want to in place the use the reference 
