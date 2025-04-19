e10ecb092e46bcf9daab910e6acf209ef4f58ecc
shitcodebykaushik <algorithmunloack@gmail.com>
2025-04-19 15:42:55 +0530

    The movement of zero at the end is done here

---
+++ leetcode/283.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        void moveZeroes(vector<int>& nums) {
+            int ptr = 0;
+            for (int i = 0; i<nums.size();i++){
+                if(nums[i]!=0){
+                    nums[ptr]=nums[i];
+                    ptr++;
+                }
+            }
+                for (int i = ptr; i<nums.size(); i++){
+                    nums[i] = 0;
+                }
+    
+            }
+        
+    };
