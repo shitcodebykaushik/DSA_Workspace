295847c73b45e5a332118fb6495741ee165d81d4
shitcodebykaushik <algorithmunloack@gmail.com>
2025-04-19 17:53:50 +0530

    All folders are in sequence till now

---
+++ Data-Structure/sets.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std ;
+int main () {
+    set<int> s = {1,20,-3,4,5,6};
+    for(auto x :s){
+        cout<<x<<" ";
+    }
+    return 0;
+}
