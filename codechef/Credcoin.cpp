6d80cd2e7f0be5cf713ef9eb5facde0b91abf090
shitcodebykaushik <algorithmunloack@gmail.com>
2025-04-26 14:02:12 +0530

    The question is easy,just a simple math with the calculation

---
+++ codechef/Credcoin.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        int c = x*y;
+        int d =static_cast<int>(c/100.0);
+        cout<<d<<endl;
+            
+        
+    }
+}
