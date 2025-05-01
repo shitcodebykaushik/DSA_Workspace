81a394e0120c89e400a215a610d067c01a319363
shitcodebykaushik <algorithmunloack@gmail.com>
2025-05-01 17:20:03 +0530

    The question was of the low rating

---
+++ codechef/billion-dollar.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+ int t;
+ cin>>t;
+ while(t--){
+    int A,B,X;
+    cin>>A>>B>>X;
+    int c = (B-A)/X;
+    cout<<c<<endl;
+ }
+}
