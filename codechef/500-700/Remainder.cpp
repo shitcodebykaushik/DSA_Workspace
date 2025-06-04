a2791beda3da87479abc8505bcdebd9fe7593f91
shiit-code <algorithmunloack@gmail.com>
2025-06-04 13:36:35 +0530

    The remainder therom is here

---
+++ codechef/500-700/Remainder.cpp
@@ -0,0 +1,15 @@
+// This is the code to find the remainder when A is divided by the B .
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t,A,B;
+cin>>t;
+while(t--){
+    cin>>A>>B;
+    int c = A%B;
+    cout<<c<<endl;
+}
+}
