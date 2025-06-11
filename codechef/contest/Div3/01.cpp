82777b9166a60430c7c58be665a76aed78945c65
shiit-code <algorithmunloack@gmail.com>
2025-06-11 20:13:03 +0530

    The qs is based on the celling divison

---
+++ codechef/contest/Div3/01.cpp
@@ -0,0 +1,13 @@
+// Learn the celling division in C++: 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+int t;
+cin>>t;
+int years_needed = ceil((25 - t+3) / 4);
+cout<<years_needed<<endl;
+}
