60eb71136af258acedbc629d81fc88f0b00ddd99
shiit-code <algorithmunloack@gmail.com>
2025-06-19 11:33:41 +0530

    The use of the reverse stl is demonstrated here

---
+++ STL/Reverse.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    // This program demonstrates the use of the reverse function in C++ with the in place .
+    vector<int> nums = {1, 2, 3, 4, 5, 6, 7};
+    // Original array
+    for(int i=0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+    cout<<endl;
+    reverse(nums.begin(),nums.end());
+    // Print the reversed array
+    for(int i =0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+    cout << endl;
+    // Reversing the array with the  the indexes 2 to 5;
+    reverse(nums.begin()+2,nums.end()-1);
+    // Print the partially reversed array
+    for(int i =0;i<nums.size();i++){
+        cout<<nums[i]<<" ";
+    }
+    return 0;
+}
