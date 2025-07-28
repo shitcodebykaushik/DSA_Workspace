824c8c388614fe730eea6a0bd54c5721d732343c
shiit-code <algorithmunloack@gmail.com>
2025-07-28 13:04:05 +0530

    The first pattern printed with the function

--- pattern/01.cpp
+++ pattern/01.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
+void func1 (int n1,int n2){
+for(int i =0;i<n1;i++){
+    for(int j=0;j<n2;j++){
+        cout<<" * "<< " ";
+    }
+    cout<<endl;
+}
+}
 int main () {
-    vector
-    
+    func1(2,5);
+    return 0;
+
 }
