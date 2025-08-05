dc1ba3b979a588435a5562ef96b2d3e8720e1387
shiit-code <algorithmunloack@gmail.com>
2025-08-05 22:51:30 +0530

    The value are updated here

---
+++ STL/vec1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int main () {
+    cout<<"--Initial vector values--"<<endl;
+    vector<int>vec(5,0); // this is the vetor with the size of 5 and initialized with 0
+    for(int i =0;i<5;i++){
+        cout<<vec[i]<<endl;
+    }
+    cout<<"This size of the vector is "<<endl;
+    cout<<vec.size()<<endl; // this will print the size of the vector
+    cout<<"This capacity of the vector is "<<endl;
+    cout<<vec.capacity()<<endl; // this will print the capacity of the vector
+    cout<<"--After adding an element to the vector in the last of it --"<<endl;
+    vec.emplace_back(4);
+    // After 
+    cout<<"The values are "<<endl;
+    for(int i =0;i<vec.size();i++){
+        cout<<vec[i]<<endl;
+    }
+cout<<"The new size is "<<vec.size()<<endl;
+cout<<"The new capacity is "<<vec.capacity()<<endl;
+    }
