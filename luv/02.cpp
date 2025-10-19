b3f4e18e715fee7ccd87916722ade1ba4c5bcf87
Kauhsik Raj <algorithmunloack@gmail.com>
2025-10-19 22:52:11 +0530

    The luv series

--- luv/02.cpp
+++ luv/02.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main () {
-    
+   // cout<<sqrt(12);
+   // Here it follow the rule of the left to right As the operator * / % are in the same table .
+   cout<<3*7/2<<endl;
+   cout<<7/2*3<<endl;
+
+
+  
 }
