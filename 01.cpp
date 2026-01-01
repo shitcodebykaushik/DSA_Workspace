8ebb2b069ec49ca2f65b891a9d4786f4e873b238
Kauhsik Raj <algorithmunloack@gmail.com>
2026-01-01 19:43:04 +0530

    The new year begin

--- 01.cpp
+++ 01.cpp
@@ -1,19 +1,24 @@
-#include <bits/stdc++.h>
+// Source - https://stackoverflow.com/q/79841435
+// Posted by Error-42, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-10, License - CC BY-SA 4.0
+
+#include <array>
+#include <iostream>
+
 using namespace std;
 
+array<int, 2> a;
+
+array<int, 2>& f() {
+    cout << "f" << endl;
+    return a;
+}
+
+int g(int const x) {
+    cout << "g " << x << endl;
+    return x;
+}
+
 int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    
-    for (int i = 0; i < n; ++i) {
-        cin >> v[i];
-    }
-    
-    for (int i = 0; i < v.size(); ++i) {
-        cout << v[i] << " ";
-    }
-    cout << "\n";
-    
-    return 0;
+    f() = { g(1), g(2) };
 }
