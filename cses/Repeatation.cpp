aebf1d9c6513b34f2169708da984f7419ceab279
Kauhsik Raj <algorithmunloack@gmail.com>
2026-01-23 10:32:14 +0530

    Implement logic to find the longest repetition in DNA sequence

--- cses/Repeatation.cpp
+++ cses/Repeatation.cpp
@@ -1,8 +1,21 @@
+//  We have the DNA sequence of the A,C,G and T . Job is to find the longest 
+// repeation in the sequence 
 #include<bits/stdc++.h>
 using namespace std;
 // Using the concept of hashing to solve this problem .
 int main () {
     string s ;
     cin>>s;
-    for(int i =0;)
+    int mx =1;
+    int cur =1;
+    for(int i =1;i<(int)s.length();i++){
+        if(s[i]==s[i-1])
+        {
+            cur++;
+            mx = max(mx,cur);
+        }else {
+            cur =1;
+        }
+    }
+    cout<<mx;
 }
