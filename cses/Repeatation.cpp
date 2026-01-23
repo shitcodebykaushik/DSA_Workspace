038c502fd27431b536f31fd758a503daa3d4d04f
Kauhsik Raj <algorithmunloack@gmail.com>
2026-01-23 10:35:59 +0530

    Refactor comments for clarity in DNA repetition algorithm

--- cses/Repeatation.cpp
+++ cses/Repeatation.cpp
@@ -3,6 +3,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 // Using the concept of hashing to solve this problem .
+// But here we can do it in a simple way
+// We will iterate through the string and keep a counter for the current
+// repeation and a variable to store the maximum repeation found so far
+// If the current character is same as the previous one we will increment
+// the counter else we will reset the counter to 1
+// Finally we will print the maximum repeation found .
 int main () {
     string s ;
     cin>>s;
