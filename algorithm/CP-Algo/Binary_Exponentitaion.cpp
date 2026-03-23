1aa1933beff59053a26621360dbdd5b3c4bf35b4
Kauhsik Raj <algorithmunloack@gmail.com>
2026-03-24 00:26:30 +0530

    First understanding is done

--- algorithm/CP-Algo/Binary_Exponentitaion.cpp
+++ algorithm/CP-Algo/Binary_Exponentitaion.cpp
@@ -1,5 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+int square (int a,int b) {
+        int result = 1; 
+        while (b>0){
+            if ( b%2 == 1 ){
+               result = result * a;
+            }
+            a =a*a;
+            b = b/2;
+        }
+            return result ;
+
+        
+    };
 int main () {
-    
+    int a = square(3,13);
+    cout<<a<<endl;
 }
