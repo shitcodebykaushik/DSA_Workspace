a1650655711f3cd5e3ccde58156a6c4ff1454a38
Kauhsik Raj <algorithmunloack@gmail.com>
2026-03-28 12:17:36 +0530

    The calculator problem is done

---
+++ algorithm/Topic-Wise/Basic/Simple-Calculator.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+    long long  x,y;
+    cin>>x>>y;
+    long long d = x+y;
+    cout<<x<<" + "<<y<<" = "<<d<<endl;
+    long long e = x*y;
+    cout<<x<<" * "<<y<<" = "<<e<<endl;
+    long long  f = x-y;
+    cout<<x<<" - "<< y<<" = "<<f<<endl;
+    
+
+}
