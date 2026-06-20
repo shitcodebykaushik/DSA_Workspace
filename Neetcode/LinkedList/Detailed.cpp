a1225c09a3c54729d7cec52c912672c157e8c4b1
Kauhsik Raj <algorithmunloack@gmail.com>
2026-06-20 14:02:29 +0530

    The linked list is done with the mannual implementation

--- Neetcode/LinkedList/Detailed.cpp
+++ Neetcode/LinkedList/Detailed.cpp
@@ -1,62 +1,92 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 class Node {
     public:
     int data;
     Node*next;
 
-    Node(int x){
-        data =x;
-        next = nullptr;
+    Node(int x) {
+    data = x;
+    next = nullptr;
     };
-  
 };
-// Defining the single linked list .
+    class Single {
+    public: 
+    Node*head;
+    Single() {
+    head=nullptr;
+    };
+    void append (int x) {
+        Node*new_node = new Node(x);
 
-class Single {
-    public:
-     Node*head ;
-     Single(){
-        head = nullptr;
-     }
-
-     // Add a node at the end 
-
-     void append (int val){
-        Node*new_node = new Node (val);
-   
-        if(head==nullptr) {
+        if(!head){
             head = new_node;
-            return;
-        };
+            return ;
+        }
+        
         Node*current = head;
-        while(current->next !=nullptr) {
+        while (current->next!=nullptr){
             current = current->next;
         }
-     }
+        current->next = new_node;
 
-
-     void display() {
+    };
+    void preppend (int x) {
+        Node*new_node = new Node(x);
+        new_node->next = head;
+        head = new_node;
+    };
+    void display () {
         Node*current = head;
-        while (current != nullptr) {
+        while (current!=nullptr){
             cout<<current->data<<" -> ";
-            current=current->next;
+            current = current->next;
         }
-        cout<<"NULL"<<endl;
-     }
+        cout<<"nullptr"<<endl;
+    };
+    // Delete a node 
+    void delethead () {
+        if (head==nullptr){
+            return;
+        }
+        Node*temp = head;
+        head = head->next;
+        delete temp;
+    };
 
-};
-
-
+    void deleteTail () {
+        if(head==nullptr){
+            return;
+        }
+        if(head->next==nullptr){
+            delete head;
+            head = nullptr;
+            return;
+        }
+        Node*current = head;
+        while(current->next->next!=nullptr){
+            current = current->next;
+        }
+        delete current->next;
+        current->next = nullptr;
+       
+    }
+    };
 int main () {
+Single s1;
+s1.append(5);
+s1.append(10);
+s1.append(20);
+s1.display();
+cout<<"After preppending: "<<endl;
+s1.preppend(1);
+s1.display();
 
+cout<<"After deleting head: "<<endl;
+s1.delethead();
+s1.display();
 
-    Single s1;
-    s1.append(1);
-  //  s1.head;
-    s1.display();
-
-
-
-};
+cout<<"After deleting tail: "<<endl;
+s1.deleteTail();
+s1.display();
+}
