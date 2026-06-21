2a27eff2a89b2eb6439f805891a6f8749a3f8980
Kauhsik Raj <algorithmunloack@gmail.com>
2026-06-22 01:00:43 +0530

    Add printReverse function to display numbers in reverse order

--- Neetcode/Recursion/Fctorail.cpp
+++ Neetcode/Recursion/Fctorail.cpp
@@ -1,13 +1,28 @@
 #include<bits/stdc++.h>
 using namespace std;
+// Use the numerical or string data type when u want to return anything 
 int factorial (int x) {
     if(x==1){
         return 1;
     }
+    // this is the recusrive thought %!=5*4!
     return x*factorial(x-1);
+};
+// Use void when u want to print something.
+void printReverse(int x) {
+    if(x==1){
+        return ;
+    }
+    cout<<x<<" ";
+    printReverse(x-1);
 };
 int main () {
     int n=5;
     int t = factorial(n);
     cout<<"The factorial of "<<t<<endl;
+
+    int f = 5;
+    printReverse(f);
+   
+
 };
